e176c5688578b612fb116a9449d5fbeb65688f57
bappaditya b <bappaditya.bandyopadhyay@gmail.com>
2021-06-10 00:34:20 +0530

    App updates

--- src/app/components/menu-home/menu-home.component.ts
+++ src/app/components/menu-home/menu-home.component.ts
@@ -12,13 +12,15 @@ export class MenuHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.menuList.push(
-      <Menu>({ title: 'Chicken Biriyani', price: '200', imageKey: 'cb', variations: 'With whole fried eggs, one potato & raita' }),
-      <Menu>({ title: 'Chicken Curry', price: '200', imageKey: 'cc', variations: 'Kosha, Do-pyaza, Butter & Rezala styled' }),
-      <Menu>({ title: 'Paneer Curry', price: '200', imageKey: 'pc', variations: 'Kosha, Do-pyaza, Butter & Rezala styled' }),
-      <Menu>({ title: 'Pulao', price: '120', imageKey: 'cc', variations: 'Jeera Rice, Basanti pulao & Peas pulao' }),
-      <Menu>({ title: 'Cholar dal', price: '100', imageKey: 'cd', variations: '' }),
-      <Menu>({ title: 'Aloo Dum', price: '100', imageKey: 'ad', variations: '' }),
-      <Menu>({ title: 'Dimer Kosha', price: '100', imageKey: 'ec', variations: 'Bengali styled egg curry' }),
+      <Menu>({ title: 'Chicken Biriyani', price: '200', veg: false ,imageKey: 'cb', variations: 'With whole fried eggs, one potato & raita' }),
+      <Menu>({ title: 'Chicken Curry', price: '200', veg: false ,imageKey: 'cc', variations: 'Kosha, Do-pyaza, Butter & Rezala styled' }),
+      <Menu>({ title: 'Paneer Curry', price: '200', veg: true ,imageKey: 'pc', variations: 'Kosha, Do-pyaza, Butter & Rezala styled' }),
+      <Menu>({ title: 'Pulao', price: '120', veg: true ,imageKey: 'pu', variations: 'Jeera Rice, Basanti pulao & Peas pulao' }),
+      <Menu>({ title: 'Cholar dal', price: '100', veg: true ,imageKey: 'cd', variations: 'Delicious pulses ' }),
+      <Menu>({ title: 'Aloo Dum', price: '100', veg: true ,imageKey: 'ad', variations: 'Juicy potato curry' }),
+      <Menu>({ title: 'Dimer Kosha', price: '100', veg: false, imageKey: 'ec', variations: 'Bengali styled egg curry' }),
+      <Menu>({ title: 'Macher Kalia', price: '150', veg: false, imageKey: 'mk', variations: 'Bengali styled fish curry' }),
+      <Menu>({ title: 'Pui chochori', price: '50',veg: true , imageKey: 'pui-c', variations: 'Bengali styled veg curry' }),
     )
   }
 
--- src/app/models/menu.ts
+++ src/app/models/menu.ts
@@ -3,4 +3,5 @@ export interface Menu {
   price: string;
   imageKey: string;
   variations: string;
+  veg: boolean;
 }
